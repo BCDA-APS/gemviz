32f33d3a180afcbbe69f1592d44d7460e0734761
Fanny Rodolakis <rodolakis@anl.gov>
2023-06-21 16:01:50 -0500

    add more complicated dictionary

--- PyQt/filter_function_test_file.py
+++ PyQt/filter_function_test_file.py
@@ -1,9 +1,9 @@
 ### I want to inport the information from the test database filter through and find key words and info
 
-from test_infomation_database import test_catalog_1
+from test_infomation_database import test_catalog_2
 
 def test_function():
-    return (test_catalog_1)
+    return (test_catalog_2)
 
 ### Run the "mainwindow.ui" file within the python file
 import pathlib
@@ -13,13 +13,18 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUi
 
-
+# this is not going to change:
 def myLoadUi(ui_file, baseinstance=None, **kw):
     """
     function to call for UI file
     """
     return loadUi(ui_file, baseinstance=baseinstance, **kw)
 
+
+
+
+
+# this is what we customize:
 class MyMainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -28,27 +33,42 @@ class MyMainWindow(QMainWindow):
         myLoadUi(ui_file, self)
         self.add_catalog_names()
         self.InputBox1.activated.connect(self.show_selected_catalog)
+        #self.InputBox1.activated.connect(self.show_selected_catalog_status)
 
-    def add_catalog_names(self):
+    def add_catalog_names(self):    # write to the pull down menu
         self.tf = test_function()
-        print(f"{self.tf.keys() = }")
+        print(f"\n{self.tf.keys() = }")
         print(f"{self.InputBox1 = }")
         self.InputBox1.clear()
         self.InputBox1.addItems(self.tf.keys())
 
-    def show_selected_catalog(self, itemnumber):
-        print(f"{itemnumber = }") 
+    def show_selected_catalog(self, itemnumber):    # read selected value from pull down menu
+        print(f"\n{itemnumber = }") 
         print(f"{self.InputBox1.currentText() = }") 
         key = self.InputBox1.currentText()  
         print(f"{self.tf[key] = }") 
-        self.statusbar.showMessage(str(self.tf[key]) + " entries")
+        self.statusbar.showMessage(str(len(self.tf[key])) + " entries")
+
+    # def show_selected_catalog_status(self, itemnumber):
+    #     print(f"\n{itemnumber = }") 
+    #     print(f"{self.InputBox1.currentText() = }") 
+    #     key = self.InputBox1.currentText()  
 
 
+
+
+
+
+
+
+
+
+# this is not going to change:
 def gui():
     """display the main widget"""
     app = QApplication(sys.argv)
     main_window = MyMainWindow()
-    print(f"{main_window.tf = }")
+    #print(f"{main_window.tf = }")
     main_window.show()
     sys.exit(app.exec_())
 
