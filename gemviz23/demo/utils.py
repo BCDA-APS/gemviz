9bfe86b772a853ce6ba1e3a9951a5375df1a2f12
Pete R Jemian <prjemian@gmail.com>
2023-06-30 12:17:15 -0500

    MNT #17 strip white space from author list

--- gemviz23/demo/utils.py
+++ gemviz23/demo/utils.py
@@ -8,19 +8,23 @@ import pathlib
 import tiled.queries
 
 ROOT_DIR = pathlib.Path(__file__).parent
+UI_DIR = ROOT_DIR / "resources"
 
 APP_DESC = "Visualize Bluesky data from tiled server."
 APP_TITLE = "GemViz23"
-AUTHOR_LIST = """
-    Ollivier Gassant
-    Fanny Rodolakis
-    Pete Jemian
-""".strip().splitlines()
+AUTHOR_LIST = [
+    s.strip()
+    for s in """
+        Ollivier Gassant
+        Fanny Rodolakis
+        Pete Jemian
+    """.strip().splitlines()
+]
+
 COPYRIGHT_TEXT = "(c) 2023, UChicago Argonne, LLC, (see LICENSE file for details)"
 DOCS_URL = "https://github.com/BCDA-APS/tiled-viz2023/blob/main/README.md"
 ISSUES_URL = "https://github.com/BCDA-APS/tiled-viz2023/issues"
 LICENSE_FILE = ROOT_DIR / "LICENSE"
-UI_DIR = ROOT_DIR / "resources"
 VERSION = "0.0.1"
 
 
