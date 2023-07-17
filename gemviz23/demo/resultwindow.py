99dd2c8ff18a441288ad4a37771d79a7a966d957
olliviergassant <ollivier.gassant@gmail.com>
2023-07-17 17:43:42 -0500

    Adding update to the status bar and buttons

--- gemviz23/demo/resultwindow.py
+++ gemviz23/demo/resultwindow.py
@@ -6,14 +6,15 @@ import sys
 class TableModel(QtCore.QAbstractTableModel):
     def __init__(self, data):
         self.columnLabels = ["Scan ID", "Plan Name", "Motors", "Detectors", "Date", "Status"]
-        self.setPageOffset(0)
-        self.setPageSize(20)
+        self.setPageOffset(0, init=True)
+        self.setPageSize(20, init=True)
         self.setAscending(True)
 
         super().__init__()
         
         self.setCatalog(data)
         self.setUidList(self._get_uidList())
+        
 
     # ------------ methods required by Qt's view
 
@@ -30,7 +31,7 @@ class TableModel(QtCore.QAbstractTableModel):
     def data(self, index, role=None):
         # display data
         if role == QtCore.Qt.DisplayRole:
-            print("Display role:", index.row(), index.column())
+            #print("Display role:", index.row(), index.column())
             uid=self.uidList()[index.row()]
             run=self.catalog()[uid]
             column=index.column()
@@ -55,8 +56,42 @@ class TableModel(QtCore.QAbstractTableModel):
                 return self.columnLabels[section]
             else:
                 return str(section + 1) #may want to alter at some point
+
+    # ------------ methods required by the results table 
+
+    def doPager(self, action, value = None):
+        print(f"doPager {action =}, {value =}")
+        if action == "first":
+            self.setPageOffset(0)
+        elif action == "pageSize":
+            self.setPageSize(value)
+        elif action == "back":
+            value = self.pageOffset() - self.pageSize()
+            value = min(value, len(self.catalog()))
+            value = max(value,0)
+            self.setPageOffset(value)
+        elif action == "next":
+            value = self.pageOffset() + self.pageSize()
+            value = min(value, len(self.catalog()) - 1 - self.pageSize())
+            value = max(value,0)
+            self.setPageOffset(value)
+        elif action == "last":
+            value = len(self.catalog()) - 1 - self.pageSize()
+            value = max(value,0)
+            self.setPageOffset(value)
+        
+        self.setUidList(self._get_uidList())
+        print(f"{self.pageOffset() =} {self.pageSize() =}")
+
+
+    def isPagerAtStart(self):
+        return self.pageOffset()==0
+
+    def isPagerAtEnd(self):
+        return (self.pageOffset() + len(self.uidList())) >= len(self.catalog())
     
     # ------------ local methods
+
     def _get_uidList(self):
         gen = self.catalog()._keys_slice(self.pageOffset(), self.pageOffset() + self.pageSize(), 1 if self.ascending() else -1)
         return list(gen)
@@ -81,17 +116,39 @@ class TableModel(QtCore.QAbstractTableModel):
     def pageSize(self):
         return self._pageSize
 
-    def setPageOffset(self, value):
-        self._pageOffset=value
+    def setPageOffset(self, offset, init=False):
+        """Set the pager offset."""
+        offset = int(offset)
+        if init:
+            self._pageOffset = offset
+        elif offset != self._pageOffset:
+            self._pageOffset = offset
+            self.layoutChanged.emit()
 
-    def setPageSize(self, value):
-        self._pageSize=value
+    def setPageSize(self, value, init=False):
+        """Set the pager size."""
+        value = int(value)
+        if init:
+            self._pageSize = value
+        elif value != self._pageSize:
+            self._pageSize = value
+            self.layoutChanged.emit()
 
     def ascending(self):
         return self._ascending
     
     def setAscending(self, value):
         self._ascending=value
+
+    def pagerStatus(self):
+        total= len(self.catalog())
+        if total==0:
+            text = "No runs"
+        else:
+            start = self.pageOffset()
+            end = start+len(self.uidList())
+            text = f"{start + 1}-{end} of {total} runs"
+        return text
     
 
 class ResultWindow(QtWidgets.QWidget):
@@ -105,10 +162,50 @@ class ResultWindow(QtWidgets.QWidget):
 
             
     def setup(self):
+        from functools import partial
+
         self.mainwindow.filter_panel.catalogs.currentTextChanged.connect(self.displayTable)
         # since we cannot set header's ResizeMode in Designer ...
         header = self.tableView.horizontalHeader()
         header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+        
+        for button_name in "first back next last".split():
+            button = getattr(self, button_name)
+            # custom: pass the button name to the receiver
+            button.released.connect(partial(self.doPagerButtons, button_name))
+        
+        self.pageSize.currentTextChanged.connect(self.doPageSize)
+        self.doButtonPermissions()
+        self.setPagerStatus()
+
+    def doPagerButtons(self, action, **kwargs):
+        print(f"{action=} {kwargs=}")
+        model = self.tableView.model()
+
+        if model is not None:
+            model.doPager(action)
+        self.doButtonPermissions()
+        self.setPagerStatus()
+    
+    def doPageSize(self, value):
+        print(f"doPageSize {value =}")
+        model = self.tableView.model()
+
+        if model is not None:
+            model.doPager("pageSize", value)
+        self.doButtonPermissions()
+        self.setPagerStatus()
+
+    def doButtonPermissions(self):
+        model = self.tableView.model()
+        atStart = False if model is None else model.isPagerAtStart()
+        atEnd = False if model is None else model.isPagerAtEnd()
+
+        self.first.setEnabled(not atStart)
+        self.back.setEnabled(not atStart)
+        self.next.setEnabled(not atEnd)
+        self.last.setEnabled(not atEnd)
+        
 
         
     def displayTable(self, *args):
@@ -117,6 +214,16 @@ class ResultWindow(QtWidgets.QWidget):
         data_model = TableModel(self.cat)
         print(f"Displaying catalog: {args[0]}")
         self.tableView.setModel(data_model)
+        self.setPagerStatus()
+
+    def setPagerStatus(self, text=None):
+        if text is None:
+            model = self.tableView.model()
+            if model is not None:
+                text=model.pagerStatus()
+
+        self.status.setText(text)
+
 
 
 # if __name__ == "__main__":
