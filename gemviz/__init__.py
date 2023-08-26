712bd590a25bbcdf0135ac29a4007d2f26c16538
Pete R Jemian <prjemian@gmail.com>
2023-08-26 13:40:19 -0500

    PKG #98 get version from git tag

--- gemviz/__init__.py
+++ gemviz/__init__.py
@@ -4,12 +4,24 @@ Define constants used throught the code.
 
 import pathlib
 
+try:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+    del get_version
+except (LookupError, ModuleNotFoundError):
+    from importlib.metadata import version
+
+    __version__ = version("pkgdemo")
+    del version
+
 __settings_orgName__ = "BCDA-APS"
 __package_name__ = "gemviz"
 
 ROOT_DIR = pathlib.Path(__file__).parent
 UI_DIR = ROOT_DIR / "resources"
 
+# TODO: get these from the pyproject.toml file?
 APP_DESC = "Visualize Bluesky data from tiled server."
 APP_TITLE = __package_name__
 AUTHOR_LIST = [
