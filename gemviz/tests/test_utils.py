2536f500da369f00810dd7f1aefb097a6ad14a2b
Pete R Jemian <prjemian@gmail.com>
2023-10-04 19:15:07 -0500

    CI #99

--- gemviz/tests/test_utils.py
+++ gemviz/tests/test_utils.py
@@ -86,6 +86,7 @@ def test_widget1(qtbot):
     """Test a simple Qt widget."""
     widget = QtWidgets.QWidget(None)
     # qtbot.addWidget(widget)
+    assert widget is not None
 
 
 def test_widget2(qtbot):
