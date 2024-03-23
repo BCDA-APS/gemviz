35ecc0bd63bdc7f5e42754912925a12d4dd8b01d
Pete R Jemian <prjemian@gmail.com>
2024-03-22 19:57:43 -0500

    MNT #215 get project parameters

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,18 +3,52 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+from importlib.metadata import version
+import pathlib
+import sys
+import tomllib
+
+sys.path.insert(0, str(pathlib.Path().absolute().parent.parent))
+import gemviz  # noqa
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "gemviz"
-copyright = "2023, Fanny Rodolakis, Pete Jemian, Ollivier Gassant"
-author = "Fanny Rodolakis, Pete Jemian, Ollivier Gassant"
-release = "0"
+root_path = pathlib.Path(__file__).parent.parent.parent
+
+with open(root_path / "pyproject.toml", "rb") as fp:
+    toml = tomllib.load(fp)
+metadata = toml["project"]
+
+gh_org = "BCDA-APS"
+project = metadata["name"]
+copyright = toml["tool"]["copyright"]["copyright"]
+author = "Ollivier Gassant, Fanny Rodolakis, Pete Jemian"
+description = metadata["description"]
+rst_prolog = f".. |author| replace:: {author}"
+github_url = f"https://github.com/{gh_org}/{project}"
+
+# -- Special handling for version numbers ------------------------------------
+# https://github.com/pypa/setuptools_scm#usage-from-sphinx
+
+release = version(project)
+version = ".".join(release.split(".")[:2])
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = """
+    sphinx.ext.autodoc
+    sphinx.ext.autosummary
+    sphinx.ext.coverage
+    sphinx.ext.githubpages
+    sphinx.ext.inheritance_diagram
+    sphinx.ext.mathjax
+    sphinx.ext.todo
+    sphinx.ext.viewcode
+""".split()
 
 templates_path = ["_templates"]
 exclude_patterns = []
